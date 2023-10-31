4d1a1b5b5965a6e87b7a31eebe27d1a9b125ca1d
Habibur Rahman <habib.swi16@gmail.com>
2023-11-01 00:57:25 +0600

    fix fetching products in order

--- src/loaders/cartProductsLoader.js
+++ src/loaders/cartProductsLoader.js
@@ -1,12 +1,18 @@
 import { getShoppingCart } from "../utilities/fakeDb";
 
 const cartProductsLoader = async () => {
-  const loadedProducts = await fetch("http://localhost:5000/products");
-  const products = await loadedProducts.json();
-
-  // if cart data is in database, you have to use async await
   const storedCart = getShoppingCart();
   const storedCartId = Object.keys(storedCart);
+
+  const loadedProducts = await fetch("http://localhost:5000/productsById", {
+    method: "POST",
+
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(storedCartId),
+  });
+  const products = await loadedProducts.json();
 
   const savedCart = [];
 
